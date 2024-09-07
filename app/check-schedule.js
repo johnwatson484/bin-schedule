72573b78713c77c054c19528b1cc0e6b7ada4b3c
John Watson <john.watson1@defra.gov.uk>
2024-09-07 14:43:20 +0100

    Restore read only filesystem

--- app/check-schedule.js
+++ app/check-schedule.js
@@ -13,7 +13,7 @@ const checkSchedule = async () => {
   options.addArguments('headless')
   options.addArguments('disable-gpu')
   options.addArguments('user-data-dir=/tmp/google-chrome')
-  options.addArguments('remote-debugging-port=9222')
+  options.addArguments('remote-debugging-pipe')
   const driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
   try {
     await driver.get('https://www.gateshead.gov.uk/article/3150/Bin-collection-day-checker')
